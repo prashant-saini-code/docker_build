68c57d22f5dc77e04121e718012fce232eb8a6a2
techdev59 <techdev@thesoftcoders.com>
2023-07-05 20:22:16 +0530

    changes

--- docker/settings.py
+++ docker/settings.py
@@ -75,15 +75,22 @@ WSGI_APPLICATION = 'docker.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ.get('DB_HOST'),
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR + "db.sqlite3",
     }
 }
 
+# postgres
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'HOST': os.environ.get('DB_HOST'),
+#         'NAME': os.environ.get('DB_NAME'),
+#         'USER': os.environ.get('DB_USER'),
+#         'PASSWORD': os.environ.get('DB_PASS'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
--- docker/urls.py
+++ docker/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 from  app import views
 urlpatterns = [
-    path(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('',views.home),
 ]
